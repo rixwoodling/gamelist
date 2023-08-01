10b3f28c1ba249ea3e04a730dc912ba49302adbb
Rix <rixwoodling@gmail.com>
2023-07-31 18:07:55 -0700

    Update gamelist.py

--- gamelist.py
+++ gamelist.py
@@ -23,7 +23,29 @@ def csv2xml():
             with open( csv_file,'r' ) as f:
                 reader = csv.DictReader( f )
                 for row in reader:
-                    output.write( '<name>'+ row['name'] +'</name>'+'\n' )
+                    name = row['name']
+                    desc = row['desc']
+                    path = './'+ row['file']
+                    image = './'+ row['file'].replace( '.zip','.png' )
+                    video = './'+ row['file'].replace( '.zip','.mp4' )
+                    genre = row['genre']
+                    releasedate = row['releasedate'].replace( '-','' )
+                    developer = row['developer']
+                    publisher = row['publisher']
+                    players = row['players']
+
+                    output.write( '<game>'+'\n' )
+                    output.write( '<name>'+ name +'</name>'+'\n' )
+                    output.write( '<desc>'+ desc + '</desc>'+'\n' )
+                    output.write( '<path>'+ path +'</path>'+'\n' )
+                    output.write( '<image>'+ image + '</image>'+'\n' )
+                    output.write( '<video>'+ video + '</video>'+'\n' )
+                    output.write( '<genre>'+ genre + '</genre>'+'\n' )
+                    output.write( '<releasedate>'+ releasedate + '</releasedate>'+'\n' )
+                    output.write( '<developer>'+ developer + '</developer>'+'\n' )
+                    output.write( '<publisher>'+ publisher + '</publisher>'+'\n' )
+                    output.write( '<players>'+ players + '</players>'+'\n' )
+                    output.write( '</game>'+'\n' )
                     output.write( '\n' )
                     print( row['name'] )
             output.close()
@@ -32,4 +54,3 @@ def csv2xml():
 
 if __name__ == "__main__":
     csv2xml()
-
